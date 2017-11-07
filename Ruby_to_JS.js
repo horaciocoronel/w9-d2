6e08feaf824bcb3c1be6d3d9ef1880154374ce61
horaciocoronel <arielc@gmail.com>
2017-11-07 14:07:53 -0500

    Finish JS warm up

--- Ruby_to_JS.js
+++ Ruby_to_JS.js
@@ -42,3 +42,94 @@ if (num > 10) {
 } else {
 console.log(num + " must be less than 10.");
 }
+
+/*
+10.times do
+  puts "Doing the same thing over and over."
+end */
+
+for (var index = 0; index < 10; index ++) {
+  var message = "Doing the same thing over and over."
+  console.log(message);
+}
+
+/*
+base = 5
+20.times do |num|
+  puts num + base
+end */
+var base = 5;
+for (var index = 0; index < 20; index ++) {
+  console.log(base + index);
+}
+
+/* total = 0
+100.times do |num|
+  total += num
+end
+puts total */
+var total = 0;
+for (var num = 0; num < 100; num ++) {
+  total += num
+}
+console.log(total);
+
+/* (3..15).each do |height|
+  if height > 9
+    puts "You can get on the rollercoaster!"
+  else
+    puts "You are too short to ride this rollercoaster."
+  end
+end */
+
+for (var num = 3; num <= 15; num ++) {
+  if (num > 9) {
+    console.log("You can get on the rollercoaster!");
+  } else {
+    console.log("You are too short to ride this rollercoaster.");
+  }
+}
+
+/*
+containers = ['purse', 'wallet', 'backback']
+containers.each do |container|
+  puts container
+end */
+var containers = ['purse', 'wallet', 'backback']
+
+for (var index = 0; index < containers.length; index ++) {
+  var item = containers[index];
+  console.log(item);
+}
+
+/*
+def hello_world
+  puts "Hello world!"
+end
+
+hello_world */
+
+function helloWorld() {
+  var myString = "Hello World!";
+  console.log(myString);
+}
+helloWorld();
+// Different option
+function helloMom() {
+  console.log("Hello Mom!");
+}
+helloMom();
+
+/* def add(first_num, second_num)
+  first_num + second_num
+end
+
+amount = add(5, 7)
+puts amount */
+
+function add(number1, number2) {
+  return number1 + number2
+}
+var ammount = add(5,7);
+console.log(ammount);
+console.log(add(5,7));// this will also work
